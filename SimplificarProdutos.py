fb51792e06e00574661a83997db371e9ab50f489
Anna Clara Damasio Monteiro <99307451+damasio23@users.noreply.github.com>
2023-11-16 18:08:48 -0300

    função SimplificarProdutos.py atualizada

--- SimplificarProdutos.py
+++ SimplificarProdutos.py
@@ -2,17 +2,31 @@ import numpy as np
 import pandas as pd
 
 def SimplificarProdutos(A, p):
-    # Criar uma cópia da matriz original
-    A_simplificado = np.copy(A)
+    # Encontrar os vértices que têm um caminho até algum material em p
+    vertices_conectados = set()
 
-    # Encontrar os vértices que não têm caminho até os materiais em p
-    vertices_a_remover = []
-    for coluna in range(A.shape[1]):
-        if coluna not in p and not any(A[:, coluna]):
-            vertices_a_remover.append(coluna)
+    # Adicionar os materiais em p
+    vertices_conectados.update(p)
 
-    # Remover os vértices da matriz
-    A_simplificado = np.delete(A_simplificado, vertices_a_remover, axis=1)
+    # Iterar até não haver mais adições aos vértices conectados
+    while True:
+        num_vertices_anterior = len(vertices_conectados)
+
+        # Iterar sobre todas as linhas da matriz A
+        for i in range(A.shape[0]):
+            # Se a linha i tem um caminho para um vértice conectado, adicionar i aos vértices conectados
+            if i not in vertices_conectados and any(A[i, j] != 0 for j in vertices_conectados):
+                vertices_conectados.add(i)
+
+        # Se não houver adições, sair do loop
+        if len(vertices_conectados) == num_vertices_anterior:
+            break
+
+    # Filtrar as linhas da matriz original com base nos vértices conectados
+    linhas_conectadas = list(vertices_conectados)
+
+    # Criar a matriz simplificada
+    A_simplificado = A[linhas_conectadas, :]
 
     return A_simplificado
 
